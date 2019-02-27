e3bcd1daab0cdde31fcb0e3df340f768c38c36e3
BaggedWater17 <47892398+BaggedWater17@users.noreply.github.com>
2019-02-26 21:26:28 -0600

    Added Hotspot support

--- stage4/02-net-tweaks/01-run.sh
+++ stage4/02-net-tweaks/01-run.sh
@@ -1,5 +1,33 @@
 #!/bin/bash -e
 
+apt-get update
+apt-get install --yes network-manager hostapd dnsmasq
+systemctl disable hostapd
+systemctl disable dnsmasq
+
+if [ -f /etc/network/interfaces ]; then
+  mv /etc/network/interfaces /etc/network/interfaces.old
+fi
+cp etc/network/interfaces /etc/network/interfaces
+
+if [ -f /etc/default/hostapd ]; then
+  mv /etc/default/hostapd /etc/default/hostapd.old
+fi
+cp etc/default/hostapd /etc/default/hostapd
+
+if [ -f /etc/hostapd/hostapd.conf ]; then
+  mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.old
+fi
+cp etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf
+
+cp etc/dnsmasq.d/rpi-access-point.conf /etc/dnsmasq.d/rpi-access-point.conf
+
+cp etc/systemd/system/rpi-access-point.service /etc/systemd/system/rpi-access-point.service
+cp usr/bin/rpi-access-point /usr/bin/rpi-access-point
+
+systemctl enable rpi-access-point.service
+
+
 install -v -d					"${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d"
 install -v -m 644 files/wait.conf		"${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/"
 
